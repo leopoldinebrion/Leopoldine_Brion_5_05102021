ad31f0fb20972fbf3b203f8eb12b899811f8ac00
Léopoldine Brion <leopoldine.brion@outlook.fr>
2021-10-29 11:32:42 +0200

    ajout modif quantité/suppression

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,19 @@
 basket = JSON.parse(localStorage.getItem('basket')) || [];
 
 //fonction qui va parcourir chaque elt du localstorage et injecter ds le DOM ses elts
-basket.forEach(element => { 
+function displayElt() {
+
+    if(basket < 1) {
+        document.querySelector('h1').innerHTML = "Votre panier est vide."
+    }
+    else {
+    basket.forEach(element => { 
 
     const parent = document.getElementById('cart__items');
     
     const totalPrice = element.price * element.quantity;
 
-    parent.innerHTML += `<article class="cart__item" data-id="${element.id}">
+    parent.innerHTML += `<article class="cart__item" data-id="${element.id}" data-color="${element.color}">
         <div class="cart__item__img">
         <img src="${element.imgSrc}" alt="${element.imgAlt}">
         </div>
@@ -28,8 +34,51 @@ basket.forEach(element => {
       </div>
     </div>
     </article>`
+    });
    
+}
+}
+
+displayElt();
+
+//////////////supprimer un article////////s////////
+function deleteItem() {
+    document.querySelectorAll('.deleteItem').forEach(element => {
+      element.addEventListener('click', (event) => {
+        event.preventDefault();
+        const newLocalStorage = basket;
+        newLocalStorage.splice(element, 1);
+        localStorage.setItem('basket',JSON.stringify(newLocalStorage));
+        location.reload();
+      });
+    });
+  }
+  deleteItem();
+
+
+/////////////modifier la quantité//////////////////
+function modifyQuantity() {
+    document.querySelectorAll('.itemQuantity').forEach(element => {
+        const id = element.closest('article').dataset.id;
+        const color = element.closest('article').dataset.color;
+        
+        basket.forEach(product => { 
+        element.addEventListener('change', (e) => {
+            e.preventDefault();
+            let newQuantity = e.target.value;
+
+            if(id === product.id && color === product.color) {
+                product.quantity = newQuantity;
+                localStorage.setItem('basket', JSON.stringify(basket));
+                location.reload;
+            }
+        });
+    });
 });
+}
+modifyQuantity();
+
+
 
 ////////////////quantité totale articles//////////////////////
 function totalQuantity() {
@@ -53,3 +102,5 @@ function totalPrice() {
 })
 }
  totalPrice();
+
+
