c3252878179b1b306140cc724e029a37d710e5a7
Léopoldine Brion <leopoldine.brion@outlook.fr>
2021-11-01 00:37:08 +0100

    ajout formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -104,3 +104,133 @@ function totalPrice() {
  totalPrice();
 
 
+///////////////FORMULAIRE/////////////////////
+/* créer fonction qui, à l'evnt click, envoi un requete post*/
+
+const form = document.querySelector('.cart__order__form');
+
+//On écoute les modifications des champs du form au changement et on appelle une fonction qui a pr paramètre ce que l'utilisateur est en train de saisir (==> this) 
+////////NOM/PRENOM///////////
+form.firstName.addEventListener('change', function() {
+  validName(this);
+});
+
+form.lastName.addEventListener('change', function() {
+  validName(this);
+});
+
+/*fonction qui vérifie si les regex st ok, si false ==> message d'erreur */
+const validName = (inputName) => {
+  const nameRegex = /^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{2,}$/;
+  let testName = nameRegex.test(inputName.value);
+
+  if(testName) {
+    inputName.nextElementSibling.innerHTML = '';
+    return true;
+    }
+  else {
+    inputName.nextElementSibling.innerHTML = 'Veuillez saisir une entrée valide';
+    return false;
+  }
+}
+
+////////EMAIL///////
+form.email.addEventListener('change', function() {
+  validEmail(this);
+});
+
+validEmail = (inputEmail) => {
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  let testEmail = emailRegex.test(inputEmail.value);
+  if(testEmail) {
+    inputEmail.nextElementSibling.innerHTML = '';
+    inputEmail.style.color = '#008000'
+    return true;
+  }
+  else {
+    inputEmail.nextElementSibling.innerHTML = "Veuillez saisir une adresse mail valide."
+    inputEmail.style.color = '#8B0000';
+    return false;
+  }
+}
+
+////////VILLE////////
+form.city.addEventListener('change', function() {
+  validCity(this);
+});
+
+validCity = (inputCity) => {
+  const cityRegex = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
+  let testCity = cityRegex.test(inputCity.value);
+  if(testCity) {
+    inputCity.nextElementSibling.innerHTML = '';
+    return true;
+  }
+  else {
+    inputCity.nextElementSibling.innerHTML = "Veuillez saisir une entrée valide."
+    inputCity.style.color = '#8B0000';
+    return false;
+  }
+}
+
+
+const btnForm = document.getElementById('order');
+
+btnForm.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if(
+    validName(form.lastName) &&
+    validName(form.firstName) &&
+    validCity(form.city) &&
+    validEmail(form.email)
+  ) {
+    console.log("niquel");
+  }
+  else {
+    console.log("champ manquant");
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//recup des données du formulaire
+
+class Form {
+    constructor() {
+      this.firstName = document.getElementById('firstName').value;
+      this.lastName = document.getElementById('lastName').value;
+      this.address = document.getElementById('address').value;
+      this.city = document.getElementById('city').value;
+      this.email = document.getElementById('email').value;
+    }
+  }
+
+const emailRegex = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+const cityRegex = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
+const nameRegex = /^[a-z ,.'-]+$/i;
+
+const emailErrorMsg = document.getElementById('emailErrorMsg');
+
+//on vérifie si les données inscrites sont bonnes
+const checkValidation = () => {
+    if(emailRegex.test(email) === false){
+        emailErrorMsg.innerHTML = "Veuillez entrer une adresse e-mail valide."
+    }
+    else {
+        return true;
+    }
+}
+
+*/
