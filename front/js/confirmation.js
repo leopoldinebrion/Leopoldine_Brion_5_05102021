1098a3810d71b3b7ae44c10ab99aa98bf4e40b94
Léopoldine Brion <leopoldine.brion@outlook.fr>
2021-11-23 10:31:47 +0100

    modif commentaire confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,7 +3,9 @@ const url = new URL(window.location.href);
 
 const orderId = url.searchParams.get("id");
 
+ //récupère la clé orderId et l'insère dans le span
 const orderIdSpan = document.getElementById('orderId');
 orderIdSpan.innerHTML = orderId;
 
+ //vider le localStorage pour ne pas que les données soient conservés
 localStorage.clear();
